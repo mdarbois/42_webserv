fca5287dee0193ec93e2d11eba6d05681460eb8c
Alexander Ehrlich <aehrlich@c4c4c1.42berlin.de>
2024-01-10 09:24:55 +0100

    add files

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,27 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Server.hpp                                         :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aehrlich <aehrlich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/10 09:07:25 by aehrlich          #+#    #+#             */
-/*   Updated: 2024/01/10 09:08:49 by aehrlich         ###   ########.fr       */
+/*   Created: 2024/01/10 09:18:24 by aehrlich          #+#    #+#             */
+/*   Updated: 2024/01/10 09:24:33 by aehrlich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SERVER_HPP
-# define SERVER_HPP
-
 #include <iostream>
-#include <cstdlib>
-#include <cstring>
-#include <unistd.h>
-#include <arpa/inet.h>
 
-class Server
+int	main(int argc, char **argv)
 {
-	
-};
+	std::string	configFile;
 
-#endif
+	if (argc == 1)
+		configFile = "Path to default config file";
+	else if (argc == 2)
+		configFile = argv[1];
+	else
+	{
+		std::cerr << "error: wrong number of arguments." << std::endl;
+		std::cerr << "Usage: ./webserv <> OR ./webserv <config.txt>" << std::endl;
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
+}
